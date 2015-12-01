04b730f675712aac3cda4d436f09fd460da59b5d
Emilio <Platzer>
2015-12-01 14:32:31 -0300

    Adapto a qa-control

--- example/server.js
+++ example/server.js
@@ -2,7 +2,7 @@
 /*jshint eqnull:true */
 /*jshint globalstrict:true */
 /*jshint node:true */
-/*eslint-env node*/
+/*eslint-disable no-console */
 
 // APP
 
@@ -12,14 +12,7 @@ var app = express();
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
 var Promises = require('best-promise');
-var fs = require('fs-promise');
-// var readYaml = require('read-yaml-promise');
-// var extensionServeStatic = require('extension-serve-static');
 var MiniTools = require('..');
-var jade = require('jade');
-
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(function(req,res,next){
     console.log('***************************');
@@ -33,23 +26,16 @@ app.use(function(req,res,next){
     next();
 });
 
-var validExts=[
-    'html',
-    'jpg','png','gif',
-    'css','js','manifest'];
 
 app.use('/index', MiniTools.serveJade('example/public/index',false));
 app.use('/public', MiniTools.serveJade('example/public',true));
 app.use('/public', MiniTools.serveStylus('example/public',true));
-// app.use('/public', extensionServeStatic('example/public', {staticExtensions:validExts}));
-
-var serveErr = MiniTools.serveErr;
 
 var actualConfig;
 
 Promises.start(function(){
     // return readYaml('example/global-config.yaml',{encoding: 'utf8'});
-}).then(function(globalConfig){
+}).then(function(/*globalConfig*/){
     /*
     actualConfig=globalConfig;
     return readYaml('example/local-config.yaml',{encoding: 'utf8'}).catch(function(err){
@@ -63,8 +49,8 @@ Promises.start(function(){
     */
     actualConfig={server: {port:3333}};
 }).then(function(){
-    return new Promises.Promise(function(resolve, reject){
-        var server=app.listen(actualConfig.server.port, function(event) {
+    return new Promises.Promise(function(resolve){
+        var server=app.listen(actualConfig.server.port, function() {
             console.log('Listening on port %d', server.address().port);
             resolve();
         });
